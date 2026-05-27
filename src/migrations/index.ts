86ee982f027c8abb1cd0954952bfeadbdfe04487
Hugo Luna <hluna88@MacBook-Pro-de-Hugo.local>
2026-05-27 14:21:29 -0600

    Test newsletter send

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260505_183843 from './20260505_183843';
+import * as migration_20260527_182830 from './20260527_182830';
 
 export const migrations = [
   {
     up: migration_20260505_183843.up,
     down: migration_20260505_183843.down,
-    name: '20260505_183843'
+    name: '20260505_183843',
+  },
+  {
+    up: migration_20260527_182830.up,
+    down: migration_20260527_182830.down,
+    name: '20260527_182830'
   },
 ];
